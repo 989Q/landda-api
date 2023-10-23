faad9aac7cec75b001fcf01e49785f227d0bf06c
SinsamutQ <99growsup@gmail.com>
2023-10-24 01:59:12 +0700

    update searchMessages

--- landda/controllers/message.ts
+++ landda/controllers/message.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 import Message from "../models/message";
-
+ 
 const sendMessage = async (req: Request, res: Response) => {
   try {
     console.log("req.body: ", req.body);
@@ -39,37 +39,52 @@ const sendMessage = async (req: Request, res: Response) => {
   }
 };
 
-export const searchMessages = async (req: Request, res: Response) => {
+export const searchMessages = async (req: any, res: Response) => {
   try {
-    const { user, keyword, sorting } = req.query;
+    const userID = req.user.userID; 
+    const { keyword, sorting } = req.query;
+
+    const user = await User.findOne({ "acc.userID": userID })
+      .populate({
+        path: "messages",
+        populate: {
+          path: "estate",
+        },
+      });
 
     if (!user) {
-      return res.status(400).json({ error: "User parameter is required" });
+      return res.status(404).json({ error: "User not found" });
     }
 
-    // Construct the query based on whether the keyword is provided
-    const query: any = { user: user as string }; // Assuming 'user' is a string, adjust accordingly
+    // Extract the messages property from the user object
+    let messages = user.messages; // Use let here to allow reassignment
 
     if (keyword) {
-      // Use $or operator to search for text, sender.email, and sender.phone
-      query.$or = [
-        { text: { $regex: keyword as string, $options: "i" } },
-        { "sender.email": { $regex: keyword as string, $options: "i" } },
-        { "sender.phone": { $regex: keyword as string, $options: "i" } },
-      ];
+      // If a keyword is provided, filter messages based on the keyword
+      const filteredMessages = messages.filter((message: any) => {
+        const messageText = message.text || "";
+        const senderEmail = message.sender.email || "";
+        const senderPhone = message.sender.phone || "";
+
+        // Check for the keyword in text, sender.email, and sender.phone
+        return (
+          messageText.includes(keyword as string) ||
+          senderEmail.includes(keyword as string) ||
+          senderPhone.includes(keyword as string)
+        );
+      });
+      
+      messages = filteredMessages;
     }
 
-    let sortOption: any = {};
+    // Sort messages based on sorting parameter
     if (sorting === "oldestDate") {
-      sortOption = { sentAt: 1 }; // Ascending order for oldest date
+      messages.sort((a: any, b: any) => a.sentAt - b.sentAt);
     } else if (sorting === "newestDate") {
-      sortOption = { sentAt: -1 }; // Descending order for newest date
+      messages.sort((a: any, b: any) => b.sentAt - a.sentAt);
     }
 
-    const messages = await Message.find(query)
-      .sort(sortOption)
-      .populate("estate");
-
+    // If no keyword is provided, return all messages
     return res.status(200).json({ messages });
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
--- landda/middlewares/validate.ts
+++ landda/middlewares/validate.ts
@@ -1,3 +1,5 @@
+// middlewares/validate.ts
+
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
@@ -17,9 +19,11 @@ export const validateToken = (req: any, res: any, next: NextFunction) => {
       token,
       process.env.ACCESS_TOKEN_SECRET as string
     ) as {
-      userID: string;
-      email: string;
-      name: string;
+      userID: string,
+      email: string,
+      image: string,
+      name: string,
+      access: string
     };
 
     req.user = decodedToken;
@@ -30,35 +34,3 @@ export const validateToken = (req: any, res: any, next: NextFunction) => {
     });
   }
 };
-
-// export const validateToken_2 = (req: any, res: any, next: NextFunction) => {
-//   let token = req.headers.authorization;
-
-//   if (!token) {
-//     return res.status(403).json({
-//       errors: [{ message: "unauthorized" }],
-//     });
-//   }
-
-//   token = token.split(" ")[1];
-
-//   try {
-//     jwt.verify(
-//       token,
-//       process.env.ACCESS_TOKEN_SECRET as string,
-//       (err: any, decoded: any) => {
-//         if (err) {
-//           return res.status(403).json({ errors: [{ message: "Forbidden" }] });
-//         }
-
-//         req.user = {
-//           userID: decoded.userID,
-//           email: decoded.email,
-//         };
-//         next();
-//       }
-//     );
-//   } catch (error) {
-//     return res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
